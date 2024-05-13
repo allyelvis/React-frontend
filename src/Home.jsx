3e0110a51aa842957bae40187c785431fd520768
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-05-13 21:18:28 +0200

    Create Home.jsx
    Home jsx

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Aenzbi</h1>
+      <p>Explore our retail and restaurant offerings!</p>
+    </div>
+  );
+}
+
+export default Home;
